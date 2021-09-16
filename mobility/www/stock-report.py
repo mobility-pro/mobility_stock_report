742dfff23f0a36eca145b1303bc59097ebfa74d0
Anupam <anupamvns0099@gmail.com>
2021-09-16 16:56:29 +0530

    feat: stock status report

--- mobility/www/stock-report.py
+++ mobility/www/stock-report.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 
 sitemap = 1
 
 def get_context(context):
+	if frappe.session.user == "Guest":
+		frappe.throw(_("Log in to access this page."), frappe.Redirect)
+
 	return context
